26940dd9c90a1e8e017e52f7ddf97d3e490c5976
Johannes Häderle <83701560+jojo20071@users.noreply.github.com>
2024-08-18 23:24:35 +0200

    7

--- chat_app.py
+++ chat_app.py
@@ -9,17 +9,24 @@ def receive_messages(sock, chat_window, chat_history):
         timestamp = datetime.datetime.now().strftime("%H:%M:%S")
         chat_history.append(f"[{timestamp}] Friend: {msg}")
         chat_window.clear()
-        chat_window.addstr("\n".join(chat_history) + "\n")
+        chat_window.border()
+        chat_window.addstr(1, 2, "\n".join(chat_history) + "\n")
         chat_window.refresh()
 
 def main(stdscr):
     curses.curs_set(1)
-    stdscr.clear()
+    curses.start_color()
+    curses.init_pair(1, curses.COLOR_CYAN, curses.COLOR_BLACK)
 
+    stdscr.clear()
     height, width = stdscr.getmaxyx()
 
-    chat_window = curses.newwin(height - 3, width, 0, 0)
-    input_window = curses.newwin(3, width, height - 3, 0)
+    chat_window = curses.newwin(height - 5, width, 0, 0)
+    input_window = curses.newwin(3, width, height - 5, 0)
+    status_window = curses.newwin(1, width, height - 2, 0)
+
+    chat_window.border()
+    input_window.border()
 
     chat_history = []
 
@@ -31,6 +38,10 @@ def main(stdscr):
     username = "You"
 
     while True:
+        status_window.clear()
+        status_window.addstr(0, 1, "Press /exit to leave the chat, /help for commands", curses.color_pair(1))
+        status_window.refresh()
+
         input_window.clear()
         input_window.addstr(1, 1, "Type your message: ")
         input_window.refresh()
@@ -47,7 +58,8 @@ def main(stdscr):
             sock.sendall(msg.encode('utf-8'))
         
         chat_window.clear()
-        chat_window.addstr("\n".join(chat_history) + "\n")
+        chat_window.border()
+        chat_window.addstr(1, 2, "\n".join(chat_history) + "\n")
         chat_window.refresh()
 
 curses.wrapper(main)
