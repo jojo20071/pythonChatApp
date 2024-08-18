1fe9939deffb6f253c05d99fe7078d5c500cdef5
Johannes Häderle <83701560+jojo20071@users.noreply.github.com>
2024-08-18 23:23:00 +0200

    2

--- chat_app.py
+++ chat_app.py
@@ -1,7 +1,7 @@
 import curses
 
 def main(stdscr):
-    curses.curs_set(0)
+    curses.curs_set(1)
     stdscr.clear()
 
     height, width = stdscr.getmaxyx()
@@ -9,11 +9,17 @@ def main(stdscr):
     chat_window = curses.newwin(height - 3, width, 0, 0)
     input_window = curses.newwin(3, width, height - 3, 0)
 
-    input_window.addstr(1, 1, "Type your message: ")
+    while True:
+        input_window.clear()
+        input_window.addstr(1, 1, "Type your message: ")
+        input_window.refresh()
 
-    chat_window.refresh()
-    input_window.refresh()
+        msg = input_window.getstr(1, 18, width - 19).decode('utf-8')
 
-    stdscr.getch()
+        if msg.lower() == "/exit":
+            break
+
+        chat_window.addstr(f"You: {msg}\n")
+        chat_window.refresh()
 
 curses.wrapper(main)
